b301322a1e66281cf8b1b73e9f60122c590ec3ec
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-21 12:43:56 +0100

    Removed unnecessary print statements and replaced by tensor operations

--- mmtrack/models/sot/siamrpn.py
+++ mmtrack/models/sot/siamrpn.py
@@ -137,44 +137,44 @@ class SiamRPN(BaseSingleObjectTracker):
             resized cropped image.
         """
         N, C, H, W = img.shape
-        context_xmin = int(center_xy[0] - crop_size / 2)
-        context_xmax = int(center_xy[0] + crop_size / 2)
-        context_ymin = int(center_xy[1] - crop_size / 2)
-        context_ymax = int(center_xy[1] + crop_size / 2)
-        _context_xmin = center_xy[0] - crop_size / 2
-        _context_xmax = center_xy[0] + crop_size / 2
-        _context_ymin = center_xy[1] - crop_size / 2
-        _context_ymax = center_xy[1] + crop_size / 2
-        print(f"context_xmin={context_xmin}, _context_xmin={_context_xmin}")
-        print(f"context_xmax={context_xmax}, _context_xmax={_context_xmax}")
-        print(f"context_ymin={context_ymin}, _context_ymin={_context_ymin}")
-        print(f"context_ymax={context_ymax}, _context_ymax={_context_ymax}")
+        # context_xmin = int(center_xy[0] - crop_size / 2)
+        # context_xmax = int(center_xy[0] + crop_size / 2)
+        # context_ymin = int(center_xy[1] - crop_size / 2)
+        # context_ymax = int(center_xy[1] + crop_size / 2)
+        context_xmin = center_xy[0] - crop_size / 2
+        context_xmax = center_xy[0] + crop_size / 2
+        context_ymin = center_xy[1] - crop_size / 2
+        context_ymax = center_xy[1] + crop_size / 2
+        # print(f"context_xmin={context_xmin}, _context_xmin={_context_xmin}")
+        # print(f"context_xmax={context_xmax}, _context_xmax={_context_xmax}")
+        # print(f"context_ymin={context_ymin}, _context_ymin={_context_ymin}")
+        # print(f"context_ymax={context_ymax}, _context_ymax={_context_ymax}")
 
-        left_pad = max(0, -context_xmin)
-        top_pad = max(0, -context_ymin)
-        right_pad = max(0, context_xmax - W)
-        bottom_pad = max(0, context_ymax - H)
-        _left_pad = torch.max(-_context_xmin, torch.tensor(0, device=img.device))
-        _top_pad = torch.max(-_context_ymin, torch.tensor(0, device=img.device))
-        _right_pad = torch.max(_context_xmax - W, torch.tensor(0, device=img.device))
-        _bottom_pad = torch.max(_context_ymax - H, torch.tensor(0, device=img.device))
-        print(f"left_pad={left_pad}, _left_pad={_left_pad}")
-        print(f"top_pad={top_pad}, _top_pad={_top_pad}")
-        print(f"right_pad={right_pad}, _right_pad={_right_pad}")
-        print(f"bottom_pad={bottom_pad}, _bottom_pad={_bottom_pad}")
+        # left_pad = max(0, -context_xmin)
+        # top_pad = max(0, -context_ymin)
+        # right_pad = max(0, context_xmax - W)
+        # bottom_pad = max(0, context_ymax - H)
+        left_pad = torch.max(-context_xmin, torch.tensor(0, device=img.device))
+        top_pad = torch.max(-context_ymin, torch.tensor(0, device=img.device))
+        right_pad = torch.max(context_xmax - W, torch.tensor(0, device=img.device))
+        bottom_pad = torch.max(context_ymax - H, torch.tensor(0, device=img.device))
+        # print(f"left_pad={left_pad}, _left_pad={_left_pad}")
+        # print(f"top_pad={top_pad}, _top_pad={_top_pad}")
+        # print(f"right_pad={right_pad}, _right_pad={_right_pad}")
+        # print(f"bottom_pad={bottom_pad}, _bottom_pad={_bottom_pad}")
 
+        # context_xmin += left_pad
+        # context_xmax += left_pad
+        # context_ymin += top_pad
+        # context_ymax += top_pad
         context_xmin += left_pad
-        context_xmax += left_pad
+        context_xmax +=  left_pad
         context_ymin += top_pad
         context_ymax += top_pad
-        _context_xmin += _left_pad
-        _context_xmax +=  _left_pad
-        _context_ymin += _top_pad
-        _context_ymax += _top_pad
-        print(f"context_xmin={context_xmin}, _context_xmin={_context_xmin}")
-        print(f"context_xmax={context_xmax}, _context_xmax={_context_xmax}")
-        print(f"context_ymin={context_ymin}, _context_ymin={_context_ymin}")
-        print(f"context_ymax={context_ymax}, _context_ymax={_context_ymax}")
+        # print(f"context_xmin={context_xmin}, _context_xmin={_context_xmin}")
+        # print(f"context_xmax={context_xmax}, _context_xmax={_context_xmax}")
+        # print(f"context_ymin={context_ymin}, _context_ymin={_context_ymin}")
+        # print(f"context_ymax={context_ymax}, _context_ymax={_context_ymax}")
 
         avg_channel = avg_channel[:, None, None]
         _avg_channel = avg_channel.expand(N, -1, -1, -1)
